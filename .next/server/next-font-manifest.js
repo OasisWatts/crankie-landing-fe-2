048b69d3d67d3462168f831cd6c6ebb92b19cb7b
OasisWatts <leahymj2@gmail.com>
2024-09-19 10:44:44 +0900

    ignore

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/oasis/programming/crankie-landing-fe-2/app/layout\":[\"static/media/21ed5661b47f7f6d-s.p.woff2\",\"static/media/c22ccc5eb58b83e1-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DrE52HQQ+lYfeqEIbUgJ33mvMhHLTAAAXvG5//fe1wc=\"\n}"
