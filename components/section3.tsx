ef4076c8b3f4baea7ef27ab936c08a6dc17dd26c
jaeyeonp <wodusqkr1210@gmail.com>
2024-09-19 10:58:00 +0900

    기본 틀 잡아버리기~

--- components/section3.tsx
+++ components/section3.tsx
@@ -11,15 +11,7 @@ export default function Section3({ data }: { data: { title1: string, title2: str
                     <Image unoptimized src={img_tmp} alt="check icon" width={50} height={50} className='rounded-sm' />
                 </div>
                 <div className="mid flex-1 flex flex-col w-full items-center justify-center mt-10">
-                    <h1 className="text-xl sm:text-3xl lg:text-4xl font-extrabold">캐스팅 공고를 올리자마자 받는</h1>
-                    <div className="flex-1 flex flex- row">
-                        <h1 className="text-xl sm:text-3xl lg:text-4xl font-extrabold text-[#2881DD]">수많은</h1>
-                        <h1 className="text-xl sm:text-3xl lg:text-4xl font-extrabold">&nbsp;배우님들의 지원 메일</h1>
-                    </div>
-                </div>
-                <div className="bot flex-1 flex flex-col items-center justify-start text-slate-400 mt-5">
-                    <p className="text-sm sm:text-base lg:text-lg font-bold">{data.paragraph1}</p>
-                    <p className="text-sm sm:text-base lg:text-lg font-bold">{data.paragraph2}</p>
+                    <h1 className="text-xl sm:text-3xl lg:text-4xl font-extrabold">새로운 영상 촬영을 앞둔 크랭이는 고민이 많습니다...</h1>
                 </div>
             </div>
             <div className="bot flex-1 w-full flex flex-col items-center mb-10 justify-center bg-white p-4 sm:p-6 lg:p-8" style={{ flexBasis: '60%' }}>
