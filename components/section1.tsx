7802e2e4de94d28d088825d784250c74adbf7cf4
fkfffk <kmw4052@naver.com>
2024-09-23 10:39:54 +0900

    657

--- components/section1.tsx
+++ components/section1.tsx
@@ -21,14 +21,14 @@ export default function Section1({ data }: { data: { title1: string, title2: str
                 className="top flex flex-col w-full items-center justify-center mt-5"
                 style={{ flexBasis: '50%' }}
             >
-                <h1 className="text-xl sm:text-3xl md:text-4xl font-extrabold text-white">
-                    {data.title1}
+                <h1 className="text-2xl sm:text-4xl md:text-5xl font-extrabold text-white"> 
+                    영화, 드라마 영상 제작사를 위한
                 </h1>
-                <h1 className="text-3xl sm:text-5xl md:text-6xl font-extrabold text-white">
-                    {data.title2}
+                <h1 className="text-2xl sm:text-4xl md:text-5xl font-extrabold text-white text-left"> 
+                   초간편 영상 제작 솔루션
                 </h1>
-                <h1 className="text-base sm:text-3xl font-extrabold text-[#A5CDF7]">
-                    빠른 프리프로덕션 - 크랭이123123
+                <h1 className="text-base sm:text-3xl font-extrabold text-[#A5CDF7] text-left"> 
+                    복잡한 문서 작성과 정리는 크랭이에게 맡기고 <br /> 당신의 창의성을 펼치세요.
                 </h1>
             </div>
             <div
